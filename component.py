6c20e5b3770107add6394eea1fecb9a7c807e71e
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-06-24 16:26:16 +0800

    test version

--- component.py
+++ component.py
@@ -1,37 +1,24 @@
 import cv2
+from typing import Iterable
 import numpy as np
 from matplotlib import pyplot as plt
 from pathlib import Path
-from tools.ccblob import ConnectedComponetBlob
+from tools.ccblob import ConnectedComponetBlob, detect_defect
 from tqdm import tqdm
 
 blober = ConnectedComponetBlob(
-    pixel_value_thr = 0,
     blob_area_lowerbound = 110, 
     min_pixels_per_comp = 70, 
-    region_mean = 20
+    region_mean = 20,
+    box_gray_lowerbound = 25
 )
 
-def detect(img:np.ndarray, return_draw=True):
-    
-    bboxs, pca, d, g = blober(img=img)
- 
-    if not return_draw:
-        if len(bboxs) == 0:
-            return 0
-        return np.where(g > 25)[0].shape[0]
-    
-    draw = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-
-    for bi, pcai, di, gi in zip(bboxs, pca, d, g):
-        print(f"peak : {pcai[0]:3d}, {pcai[1]:3d}/{pcai[2]:3d}= {di:.3f}, L={gi:.3f}")
-        if gi > 25:
-            cv2.rectangle(
-                draw, (bi[1], bi[0]), (bi[3], bi[2]),
-                color=(0,0,255), thickness = 1
-            )
-    return draw
-
+noob_blober =  ConnectedComponetBlob(
+    blob_area_lowerbound = 110, 
+    min_pixels_per_comp = 0, 
+    region_mean = 0,
+    box_gray_lowerbound = 0
+)
 
 def check_error_only():
     error_table = []
@@ -45,8 +32,8 @@ def check_error_only():
             #    str(dst/f"{im.stem}.jpg"), 
             #    detect(img = cv2.imread(str(im), cv2.IMREAD_GRAYSCALE))
             #)
-        x = detect(img = cv2.imread(str(Path(im)), cv2.IMREAD_GRAYSCALE))
-        if x == 0:
+        bboxs, pca, d, g, crops, draw = detect_defect(blober=blober, img = cv2.imread(str(Path(im)), cv2.IMREAD_GRAYSCALE))
+        if len(bboxs) == 0:
             print(f"Error {im}")
     
 def overall():
@@ -58,17 +45,25 @@ def overall():
             target_img = [_ for _ in target.glob("*.jpg") if 'aug' not in _.stem]
             target_img.sort(key=lambda x:int(x.stem))
             for im in tqdm(target_img):
-                x = detect(img = cv2.imread(str(im), cv2.IMREAD_GRAYSCALE), return_draw=False)
+                bboxs, pca, d, g, crops= detect_defect(
+                    blober=blober, 
+                    img = cv2.imread(str(im), cv2.IMREAD_GRAYSCALE), 
+                    return_draw=False
+                )
+
                 if t == 0:
-                    if x > 0:
+                    if len(bboxs) > 0:
                         print(f"Error {im}", file=log, flush=True)
                 else:
-                    if x == 0:
+                    if len(bboxs) == 0:
                         print(f"Error {im}", file=log, flush=True)
                     
 if __name__ == "__main__":
-    overall()
+    #overall()
     #check_error_only()
-    #x = detect(img=cv2.imread("dataset\\p2\\Type2\\128.jpg", cv2.IMREAD_GRAYSCALE))
+    bboxs, pca, d, g, crops, draw = detect_defect(
+        img=cv2.imread("dataset/p2/Type2/11.jpg", cv2.IMREAD_GRAYSCALE),
+        blober=blober
+    )
 
-    #cv2.imwrite("v.jpg", x)
+    cv2.imwrite("v.jpg", draw)
