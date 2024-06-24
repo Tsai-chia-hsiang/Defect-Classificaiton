5a6b2344d20ef3d9fbfeed071433f35b323e2acc
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-06-24 14:36:40 +0800

    test

--- component.py
+++ component.py
@@ -1,23 +1,58 @@
 import cv2
-import numpy as np
-from matplotlib import pyplot as plt
+import pandas as pd
+from tqdm import tqdm
 from pathlib import Path
+import numpy as np
+from tools import ccblob
+from tools.io import write_json
 
-image_path =Path("Type1")/"((Confidential))210509_090939_21L4C22DAY-S22L-VF12-C04_S02_REJECTED.jpg"
-image = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
-print(image.shape)
-# Thresholding to find blobs
-# Thresholding to find blobs
-_, thresh = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+def get_a_type_imgs(root:Path, t:str)->list[Path]:
+    r= [_ for _ in (root/t).glob("*.jpg") if 'aug' not in _.stem]
+    r.sort(key=lambda x:int(x.stem))
+    return r
 
-thresh = thresh[1:-1, 1:-1, ...]
-cv2.imwrite("t.jpg",thresh)
+blober = ccblob.ConnectedComponetBlob(
+    blob_area_lowerbound = 180,
+    peak_lowerbound = 180,
+    density_lowerbound = 0.5
+)
 
-# Find connected components
-num_labels, labels_im = cv2.connectedComponents(thresh)
-# Display the result
-plt.figure(dpi=600)
-plt.imshow(labels_im, cmap='nipy_spectral')
-plt.axis('off')
-plt.show()
-num_labels
+def unit_test(img_path:str) -> tuple[int, int]:
+    img = cv2.imread(img_path , cv2.IMREAD_GRAYSCALE)
+    bboxes, pca_lc, d = blober(img=img)
+    # print(bboxes)
+    if len(bboxes) == 0:
+        return 0 ,0
+    
+    max_area = pca[:, 2].max()
+    if max_area < 500:
+        return 0, 0
+    return len(bboxes), int(max_area)
+
+
+def main():
+    dataroot = Path("dataset")/"p2"
+    target_type = [f"Type0", "Type1", "Type2", "Type3"]
+    
+    img_all:list[Path] = []
+    for ti in target_type:
+        img_all +=  get_a_type_imgs(dataroot, ti)
+
+    log = open("ccp_wa.csv", "w+")
+    print("files,nbox,gt,mex_area", file=log)
+    pbar = tqdm(img_all)
+    error_pred = 0
+    for img_path in pbar:
+        defect = int(img_path.parts[-2][-1]) > 0
+        nbox, max_a = unit_test(img_path=str(img_path))
+        is_defect = nbox > 0
+        if is_defect != defect:
+            error_pred += 1
+            print(f"{str(img_path)},{nbox},{int(defect)},{max_a}", file=log, flush=True)
+        pbar.set_postfix(ordered_dict={'error':error_pred})
+    log.close()
+
+if __name__ == "__main__":
+    main()
+    #nbox, a = unit_test(img_path="dataset/p2/Type2/218.jpg")
+    #print(a)
