288bde3863263465fc34bf50d6f6f5da0887ce4a
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-06-24 14:46:41 +0800

    current best

--- component.py
+++ component.py
@@ -1,58 +1,74 @@
 import cv2
-import pandas as pd
-from tqdm import tqdm
-from pathlib import Path
 import numpy as np
-from tools import ccblob
-from tools.io import write_json
+from matplotlib import pyplot as plt
+from pathlib import Path
+from tools.ccblob import ConnectedComponetBlob
+from tqdm import tqdm
 
-def get_a_type_imgs(root:Path, t:str)->list[Path]:
-    r= [_ for _ in (root/t).glob("*.jpg") if 'aug' not in _.stem]
-    r.sort(key=lambda x:int(x.stem))
-    return r
-
-blober = ccblob.ConnectedComponetBlob(
-    blob_area_lowerbound = 180,
-    peak_lowerbound = 180,
-    density_lowerbound = 0.5
+blober = ConnectedComponetBlob(
+    pixel_value_thr = 0,
+    blob_area_lowerbound = 110, 
+    min_pixels_per_comp = 70, 
+    region_mean = 20
 )
 
-def unit_test(img_path:str) -> tuple[int, int]:
-    img = cv2.imread(img_path , cv2.IMREAD_GRAYSCALE)
-    bboxes, pca_lc, d = blober(img=img)
-    # print(bboxes)
-    if len(bboxes) == 0:
-        return 0 ,0
+def detect(img:np.ndarray, return_draw=True):
     
-    max_area = pca[:, 2].max()
-    if max_area < 500:
-        return 0, 0
-    return len(bboxes), int(max_area)
-
-
-def main():
-    dataroot = Path("dataset")/"p2"
-    target_type = [f"Type0", "Type1", "Type2", "Type3"]
+    bboxs, pca, d, g = blober(img=img)
+ 
+    if not return_draw:
+        if len(bboxs) == 0:
+            return 0
+        return np.where(g > 25)[0].shape[0]
     
-    img_all:list[Path] = []
-    for ti in target_type:
-        img_all +=  get_a_type_imgs(dataroot, ti)
+    draw = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-    log = open("ccp_wa.csv", "w+")
-    print("files,nbox,gt,mex_area", file=log)
-    pbar = tqdm(img_all)
-    error_pred = 0
-    for img_path in pbar:
-        defect = int(img_path.parts[-2][-1]) > 0
-        nbox, max_a = unit_test(img_path=str(img_path))
-        is_defect = nbox > 0
-        if is_defect != defect:
-            error_pred += 1
-            print(f"{str(img_path)},{nbox},{int(defect)},{max_a}", file=log, flush=True)
-        pbar.set_postfix(ordered_dict={'error':error_pred})
-    log.close()
+    for bi, pcai, di, gi in zip(bboxs, pca, d, g):
+        print(f"peak : {pcai[0]:3d}, {pcai[1]:3d}/{pcai[2]:3d}= {di:.3f}, L={gi:.3f}")
+        if gi > 25:
+            cv2.rectangle(
+                draw, (bi[1], bi[0]), (bi[3], bi[2]),
+                color=(0,0,255), thickness = 1
+            )
+    return draw
 
+
+def check_error_only():
+    error_table = []
+    with open("loss.txt", "r") as f:
+        for li in f.readlines():
+            error_table .append(li.strip())
+    
+    for im in tqdm(error_table):
+            
+            #cv2.imwrite(
+            #    str(dst/f"{im.stem}.jpg"), 
+            #    detect(img = cv2.imread(str(im), cv2.IMREAD_GRAYSCALE))
+            #)
+        x = detect(img = cv2.imread(str(Path(im)), cv2.IMREAD_GRAYSCALE))
+        if x == 0:
+            print(f"Error {im}")
+    
+def overall():
+    with open("e.log", "w+", encoding="utf-8") as log:
+        for t in range(1, 4):
+            target = Path("dataset")/"p2"/f"Type{t}"
+            dst = Path("dataset")/"det"/f"Type{t}"
+            dst.mkdir(parents=True, exist_ok=True)
+            target_img = [_ for _ in target.glob("*.jpg") if 'aug' not in _.stem]
+            target_img.sort(key=lambda x:int(x.stem))
+            for im in tqdm(target_img):
+                x = detect(img = cv2.imread(str(im), cv2.IMREAD_GRAYSCALE), return_draw=False)
+                if t == 0:
+                    if x > 0:
+                        print(f"Error {im}", file=log, flush=True)
+                else:
+                    if x == 0:
+                        print(f"Error {im}", file=log, flush=True)
+                    
 if __name__ == "__main__":
-    main()
-    #nbox, a = unit_test(img_path="dataset/p2/Type2/218.jpg")
-    #print(a)
+    overall()
+    #check_error_only()
+    #x = detect(img=cv2.imread("dataset\\p2\\Type2\\128.jpg", cv2.IMREAD_GRAYSCALE))
+
+    #cv2.imwrite("v.jpg", x)
