66693540b146d621692482f6101100a81ac93fda
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-06-29 22:12:55 +0800

    to merge

--- train_rn.sh
+++ train_rn.sh
@@ -1,5 +1,5 @@
-CKPT_DIR=./ckpt/rn_50/
-DATA_TABLE=./dataset/5_folds.json
+CKPT_DIR=./ckpt/rn_50_w_down/
+DATA_TABLE=./dataset/down_5_folds.json
 LABEL_MAP=./dataset/label.json
 MODEL=resnet50
 
@@ -7,4 +7,4 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 python train_test.py \
     --data_table $DATA_TABLE --label_map $LABEL_MAP\
     --batchsize 60  --lr 0.001 --epochs 50\
     --ckpt_dir $CKPT_DIR\
-    --no_valid
+    --weight_loss
