9b8d7d7cfcfd7e045d28503338e7fa4dea8afb36
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-07-02 19:33:38 +0800

    adding coordinate-attached training cmd

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,4 +1,4 @@
-CKPT_DIR=./ckpt/patch
+CKPT_DIR=./ckpt/patch_test
 TABLE_ROOT=./connectedcomponents/patches
 LABEL_MAP=./dataset/label.json 
 MODEL=resnet34
@@ -17,11 +17,51 @@ if ! [ -d "$CKPT_DIR/baseline34" ]; then
     mkdir "$CKPT_DIR/baseline34"
 fi
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python $MAIN_SCRIPT \
+CUDA_VISIBLE_DEVICES=4,5,6,7 python $MAIN_SCRIPT \
     --data_table $TABLE_ROOT/f_table.json --label_map $LABEL_MAP \
-    --patch --using_model $MODEL \
+    --dtype patchimg --using_model $MODEL \
     --batchsize $BATCHSIZE  --lr $LR --epochs $EPOCHS --patient $PATIENT\
     --ckpt_dir $CKPT_DIR/baseline34 \
     --train --test > $CKPT_DIR/baseline34/train_test.log
 
 python texture_classify.py > $CKPT_DIR/baseline34/final.log
+
+if ! [ -d "$CKPT_DIR/baseline_coo34" ]; then
+        echo "Directory '$CKPT_DIR/baseline_coo34' does not exist. Creating it now."
+        mkdir "$CKPT_DIR/baseline_coo34"
+fi
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python $MAIN_SCRIPT \
+    --data_table $TABLE_ROOT/patches_coo.json --label_map $LABEL_MAP \
+    --dtype patchimg --using_model $MODEL \
+    --batchsize $BATCHSIZE  --lr $LR --epochs $EPOCHS --patient $PATIENT\
+    --ckpt_dir $CKPT_DIR/baseline_coo34 \
+    --train --test > $CKPT_DIR/baseline_coo34/train_test.log
+python texture_classify.py --predict_csv  $CKPT_DIR/baseline_coo34/test_pred.csv > $CKPT_DIR/baseline_coo34/final.log
+
+if ! [ -d "$CKPT_DIR/baseline_coo34_wce" ]; then
+    echo "Directory '$CKPT_DIR/baseline_coo34_wce' does not exist. Creating it now."
+    mkdir "$CKPT_DIR/baseline_coo34_wce"
+fi
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python $MAIN_SCRIPT \
+    --data_table $TABLE_ROOT/patches_coo.json --label_map $LABEL_MAP \
+    --dtype patchimg --using_model $MODEL \
+    --batchsize $BATCHSIZE  --lr $LR --epochs $EPOCHS --patient $PATIENT\
+    --weight_loss --ckpt_dir $CKPT_DIR/baseline_coo34_wce \
+    --train --test > $CKPT_DIR/baseline_coo34_wce/train_test.log
+
+python texture_classify.py --predict_csv  $CKPT_DIR/baseline_coo34_wce/test_pred.csv > $CKPT_DIR/baseline_coo34_wce/final.log
+
+if ! [ -d "$CKPT_DIR/baseline_coo34_focal" ]; then
+        echo "Directory '$CKPT_DIR/baseline_coo34_focal' does not exist. Creating it now."
+        mkdir "$CKPT_DIR/baseline_coo34_focal"
+fi
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python $MAIN_SCRIPT \
+    --data_table $TABLE_ROOT/patches_coo.json --label_map $LABEL_MAP \
+    --dtype patchimg --using_model $MODEL \
+    --batchsize $BATCHSIZE  --lr $LR --epochs $EPOCHS --patient $PATIENT\
+    --weight_loss --loss focal --ckpt_dir $CKPT_DIR/baseline_coo34_focal \
+    --train --test > $CKPT_DIR/baseline_coo34_focal/train_test.log
+python texture_classify.py --predict_csv  $CKPT_DIR/baseline_coo34_focal/test_pred.csv > $CKPT_DIR/baseline_coo34_focal/final.log
