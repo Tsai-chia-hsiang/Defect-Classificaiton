1038251d30d31889d2e653eb7085ba559bc3592b
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-07-08 23:57:05 +0800

    independent patch training

---
+++ scripts/patch_coo_pos.sh
@@ -0,0 +1,29 @@
+CKPT_DIR=./ckpt/release/patch_independent
+TABLE_ROOT=./table/patches
+LABEL_MAP=./table/label.json 
+MODEL=resnet34
+LR=0.0005
+PATIENT=30
+BATCHSIZE=256
+EPOCHS=50
+MAIN_SCRIPT=train_test.py
+
+if ! [ -d "$CKPT_DIR" ]; then
+    echo "Directory '$CKPT_DIR' does not exist. Creating it now."
+    mkdir "$CKPT_DIR"
+fi
+WD=$CKPT_DIR/pos
+if ! [ -d $WD ]; then
+    echo "Directory '$WD' does not exist. Creating it now."
+    mkdir "$WD"
+fi
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python $MAIN_SCRIPT \
+    --data_table $TABLE_ROOT/patches.json --label_map $LABEL_MAP \
+    --dtype patch --patch_independent \
+    --coor pos --using_model $MODEL \
+    --batchsize $BATCHSIZE  --lr $LR --epochs $EPOCHS --patient $PATIENT\
+    --ckpt_dir $WD \
+    --train --test > $WD/train_test.log
+
+python texture_classify.py --predict_csv $WD/test_pred.csv > $WD/full_test.log
