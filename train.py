1b339a050ffe35b5a464651100f199fafb46333a
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-08-06 09:49:13 +0800

    rename from train_test.py due to it currently only do train

---
+++ train.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+import argparse
+from time import time
+import torch
+from tools.torchtools import set_seed
+set_seed(seed=891122)
+from tools.dataset import build_datasets, patch_agg_collate_fn
+from tools.io import read_json
+from model import MODEL_MAP
+from model.classification import train
+
+def parsing():
+    p = argparse.ArgumentParser()
+    
+    # dataset metadata
+    p.add_argument("--data_table", type=Path, default=Path("table")/"release_train.json")
+    p.add_argument("--label_map", type=Path, default=Path("table")/"label.json")
+    p.add_argument("--dtype", type=str, default="fullimg")
+    p.add_argument("--patch_independent", action='store_true')
+    p.add_argument("--coor", type=str, default="pos")
+
+    # hyper parameters
+    p.add_argument("--epochs", type=int, default=50)
+    p.add_argument("--batchsize", type=int, default=40)
+    p.add_argument("--lr", type=float, default=1e-3)
+    p.add_argument("--patient", type=int, default=10)
+    
+    # model saving path
+    p.add_argument("--ckpt_dir", type=Path, default=Path("ckpt")/"baseline")
+    p.add_argument("--using_model", type=str, default="resnet50")
+    
+    
+    # classification loss
+    p.add_argument("--loss", type=str, default="ce")
+    p.add_argument("--focal_gamma", type=float, default=2)
+    p.add_argument("--weight_loss", action='store_true')
+
+    
+    args = p.parse_args()
+    return args
+
+if __name__ == "__main__":
+    
+    args = parsing()
+    
+    ckpt_dir = Path(args.ckpt_dir)
+    ckpt_dir.mkdir(parents=True, exist_ok=True)
+    
+    # prepare dataset 
+    label_map = read_json(args.label_map)
+    data_table = read_json(args.data_table)
+    print(f"from {args.data_table} reading all data")
+    print(f"class-label map: {label_map}")
+
+    print(f"settings :")
+    print(f"lr: {args.lr}, epochs : {args.epochs}, batchsize : {args.batchsize}")
+
+    dataset = build_datasets(
+        file_table = data_table, label_map = label_map, 
+        coor_using=args.coor, dtype=args.dtype, 
+        patch_independent=args.patch_independent,
+        src_wh=[763, 463]
+    )
+    
+    model:torch.nn.Module = None
+    contain_coo = dataset['train'].contain_coo
+
+    if 'resnet' in args.using_model:
+        depth = args.using_model[6:]
+        model = MODEL_MAP['resnet'](
+            grayscale=True, ncls=len(label_map), 
+            coo = contain_coo, encoder_depth = depth,
+            pos_emd = args.coor == "pos", 
+            patch_agg = not args.patch_independent
+        )
+
+ 
+    collect_fn = None 
+    if args.dtype == 'patch' and not args.patch_independent :
+        collect_fn = patch_agg_collate_fn
+    print(collect_fn)
+    model_name = f"{args.using_model}.pt"
+
+    print(f"model will be saved at {ckpt_dir/model_name}")
+    s = time()
+    train(
+        model = model, dataset=dataset, loader_collection=collect_fn, 
+        ckpt_dir=ckpt_dir, model_name=model_name,
+        cls_loss = args.loss, focal_gamma=args.focal_gamma,
+        epochs=args.epochs, batchsize=args.batchsize, lr=args.lr, early_stop=args.patient,
+        loss_weight={
+            'train':dataset['train'].cls_w if args.weight_loss else None,
+            'valid':dataset['valid'].cls_w if args.weight_loss else None
+        },
+        return_model=False
+    )
+    t = time()
+    using_time = (t-s)/60
+    print(f"training using {using_time} mins ({using_time/60} hours).")
+        
