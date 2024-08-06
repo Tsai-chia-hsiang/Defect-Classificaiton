71ca89e5734ad9144c60206e1bf56a1cd5ae23c2
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-08-06 10:00:09 +0800

    doing release dataset

--- scripts/fullimg_last4gpus.sh
+++ scripts/fullimg_last4gpus.sh
@@ -1,64 +1,26 @@
-CKPT_DIR=./ckpt/test
-TABLE_ROOT=./table/pretest
+CKPT_DIR=./ckpt/release_simi
+TABLE_ROOT=./table/release_simi
 LABEL_MAP=./table/label.json
 MODEL=resnet50
 LR=0.001
 PATIENT=30
 BATCHSIZE=68
 EPOCHS=50
-MAIN_SCRIPT=train_test.py
+MAIN_SCRIPT=train.py
 
 if ! [ -d "$CKPT_DIR" ]; then
     echo "Directory '$CKPT_DIR' does not exist. Creating it now."
     mkdir "$CKPT_DIR"
 fi
 
-if ! [ -d "$CKPT_DIR/baseline" ]; then
-    echo "Directory '$CKPT_DIR/baseline' does not exist. Creating it now."
-    mkdir "$CKPT_DIR/baseline"
+if ! [ -d "$CKPT_DIR/wce" ]; then
+    echo "Directory '$CKPT_DIR/wce' does not exist. Creating it now."
+    mkdir "$CKPT_DIR/wce"
 fi
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 python $MAIN_SCRIPT \
-    --data_table $TABLE_ROOT/baseline.json --label_map $LABEL_MAP \
+    --data_table $TABLE_ROOT/realse_0half.json --label_map $LABEL_MAP \
     --batchsize $BATCHSIZE  --lr $LR --epochs $EPOCHS --patient $PATIENT\
-    --ckpt_dir $CKPT_DIR/baseline \
-    --train --test > $CKPT_DIR/baseline/train_test.log
-
-
-if ! [ -d "$CKPT_DIR/baseline_wce" ]; then
-    echo "Directory '$CKPT_DIR/baseline_wce' does not exist. Creating it now."
-    mkdir "$CKPT_DIR/baseline_wce"
-fi
-
-CUDA_VISIBLE_DEVICES=4,5,6,7 python $MAIN_SCRIPT \
-    --data_table $TABLE_ROOT/baseline.json --label_map $LABEL_MAP \
-    --batchsize $BATCHSIZE  --lr $LR --epochs $EPOCHS --patient $PATIENT\
-    --ckpt_dir $CKPT_DIR/baseline_wce \
+    --ckpt_dir $CKPT_DIR/wce \
     --weight_loss \
-    --train --test > $CKPT_DIR/baseline_wce/train_test.log
-
-if ! [ -d "$CKPT_DIR/aug_wce" ]; then
-    echo "Directory '$CKPT_DIR/aug_wce' does not exist. Creating it now."
-    mkdir "$CKPT_DIR/aug_wce"
-fi
-
-CUDA_VISIBLE_DEVICES=4,5,6,7 python $MAIN_SCRIPT \
-    --data_table $TABLE_ROOT/aug.json --label_map $LABEL_MAP \
-    --batchsize $BATCHSIZE  --lr $LR --epochs $EPOCHS --patient $PATIENT\
-    --ckpt_dir $CKPT_DIR/aug_wce \
-    --weight_loss \
-    --train --test > $CKPT_DIR/aug_wce/train_test.log
-
-
-
-if ! [ -d "$CKPT_DIR/down_wce" ]; then
-    echo "Directory '$CKPT_DIR/down_wce' does not exist. Creating it now."
-    mkdir "$CKPT_DIR/down_wce"
-fi
-
-CUDA_VISIBLE_DEVICES=4,5,6,7 python $MAIN_SCRIPT \
-    --data_table $TABLE_ROOT/down.json --label_map $LABEL_MAP \
-    --batchsize $BATCHSIZE  --lr $LR --epochs $EPOCHS --patient $PATIENT\
-    --ckpt_dir $CKPT_DIR/down_wce \
-    --weight_loss \
-    --train --test > $CKPT_DIR/down_wce/train_test.log
+    --train > $CKPT_DIR/wce/train.log
